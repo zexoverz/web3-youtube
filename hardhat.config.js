2deef3ae2d554e72813ffd17f79cdc492922d097
faisal <faisalfirdani01@gmail.com>
2023-10-18 22:23:17 +0700

    Develop Youtube Smart Contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY,]
+    },
+  },
+  paths: {
+    artifacts: "./artifacts",
+  },
 };
