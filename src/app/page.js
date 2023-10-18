414231c1a317341555de2e86684d568b7313d52e
faisal <faisalfirdani01@gmail.com>
2023-10-18 21:55:46 +0700

    Set Up Project

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-6xl font-bold text-slate-900">Web3 YouTube Clone</h1>
+      <h3 className="text-2xl mt-8 text-slate-900">
+        Next.js, TailwindCSS, Solidity, IPFS, The Graph and Polygon
+      </h3>
+    </div>
+  )
+}
