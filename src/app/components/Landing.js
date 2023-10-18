1953618341c718e38efbff29209ef6cedf0e59a8
faisal <faisalfirdani01@gmail.com>
2023-10-18 22:57:39 +0700

    Create Auth Page

---
+++ src/app/components/Landing.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+function Landing() {
+  // Creating a function to connect user's wallet
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      // Checking if user have Metamask installed
+      if (!ethereum) {
+        // If user doesn't have Metamask installed, throw an error
+        alert("Please install MetaMask");
+        return;
+      }
+
+      // If user has Metamask installed, connect to the user's wallet
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      // At last save the user's wallet address in browser's local storage
+      localStorage.setItem("walletAddress", accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      {/* Creating a hero component with black background and centering everything in the screen */}
+      <section className="relative bg-black flex flex-col h-screen justify-center items-center">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6">
+          <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+            <div className="text-center pb-12 md:pb-16">
+              <h1
+                className="text-5xl text-white md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
+                data-aos="zoom-y-out"
+              >
+                It is YouTube, but{" "}
+                <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+                  Decentralized
+                </span>
+              </h1>
+              <div className="max-w-3xl mx-auto">
+                <p
+                  className="text-xl text-gray-400 mb-8"
+                  data-aos="zoom-y-out"
+                  data-aos-delay="150"
+                >
+                  A YouTube Clone built on top of Polygon network, allow users
+                  to create, share and watch videos, without worrying about
+                  their privacy.
+                </p>
+                <button
+                  className="items-center  bg-white rounded-full font-medium  p-4 shadow-lg"
+                  onClick={() => {
+                    // Calling the connectWallet function when user clicks on the button
+                    connectWallet();
+                  }}
+                >
+                  <span>Connect wallet</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Landing;
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,9 @@
-import Image from 'next/image'
+'use client'
+
+import Landing from './components/Landing'
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-6xl font-bold text-slate-900">Web3 YouTube Clone</h1>
-      <h3 className="text-2xl mt-8 text-slate-900">
-        Next.js, TailwindCSS, Solidity, IPFS, The Graph and Polygon
-      </h3>
-    </div>
+    <Landing />
   )
 }
