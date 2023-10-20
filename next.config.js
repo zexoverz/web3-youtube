bece394cf93d5caba905064214af8ce14578aad1
faisal <faisalfirdani01@gmail.com>
2023-10-20 19:07:03 +0700

    Set Up IPFS Upload

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        WEB3_STORAGE_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDg2MjU4NjM1M0FjYTQxMjYzMDIwYmU5NzEwNDczYUYxQzU5NThkOTgiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2OTc4MDMyNjk2MjUsIm5hbWUiOiJvbmJvYXJkIn0.BvV-sSOF8s507ieqkTrXdqYD682z3piqXqW1e60Qbkk"
+    }
+}
 
 module.exports = nextConfig
